c3ceeec762ea75cda0b604a9607108b78c9e1c4d
Muqtadirat <muqtadir.my@gmail.com>
2024-02-28 20:50:10 +0100

    chore: icons

--- src/icons/index.jsx
+++ src/icons/index.jsx
@@ -6,3 +6,5 @@ export { default as CalendarTime } from "./calendar-time";
 export { default as Trashcan } from "./trash";
 export { default as Circle } from "./circle";
 export { default as Pen } from "./pen";
+export { default as XIcon } from "./close"
+export {default as Info} from "./info"
