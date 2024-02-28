44f8758bed16ec5147b4d764651d41a3c5dadbd2
Muqtadirat <muqtadir.my@gmail.com>
2024-02-28 13:43:57 +0100

    chore: icon

--- src/icons/index.jsx
+++ src/icons/index.jsx
@@ -1,1 +1,8 @@
-export {default as Clipboard} from "./clipboard"
+export { default as Clipboard } from "./clipboard";
+export { default as Calendar } from "./calendar";
+export { default as CalendarX } from "./calendar-x";
+export { default as CalendarChecked } from "./calendar-check";
+export { default as CalendarTime } from "./calendar-time";
+export { default as Trashcan } from "./trash";
+export { default as Circle } from "./circle";
+export { default as Pen } from "./pen";
